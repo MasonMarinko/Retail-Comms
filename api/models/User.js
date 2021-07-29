cf2ac8c7ce29eca522b0705d41dc11dba0acc797
MasonM <Mason.P.Marinko@gmail.com>
2021-07-28 22:03:45 -0600

    hashed password, next step will be comparing using bcrypt for login to work

--- api/models/User.js
+++ api/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const Types = Schema.Types
+const bcrypt = require('bcrypt')
 
 const userSchema = new Schema({
     // author: { type: 'ObjectId', ref: 'Person' }
@@ -35,5 +36,17 @@ userSchema.methods={
         return this.password == password
     }
 }
+
+userSchema.pre('save', async function (next) {
+    try {
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(this.password, salt)
+        this.password = hashedPassword
+        next()
+    } catch (error) {
+        next(error)
+    }
+})
+
 const User = mongoose.model('user', userSchema);
 module.exports = User
